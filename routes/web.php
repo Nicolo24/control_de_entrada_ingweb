136e86b68f47a806d9f4e674ce67753c8c951a79
Nicolas Cabezas <nicolas.cabezas@udla.edu.ec>
2022-05-15 16:43:17 -0500

    ok

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('users', [CostumerController::class]);
+Route::resource('users', [UserController::class]);
