0bba6fccd2f6eb3b02ed79c5f959df64750b7f19
Nicolas Cabezas <nicolas.cabezas@udla.edu.ec>
2022-05-26 18:54:41 -0500

    ok

---
+++ database/migrations/2022_05_26_235014_casas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('casas', function (Blueprint $table) {
+            $table->id();
+            $table->string('friendly_id')->nullable();
+            $table->float('latitude')->nullable();
+            $table->float('longitude')->nullable();
+            $table->string('phone')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
